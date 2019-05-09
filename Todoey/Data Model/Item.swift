13d5d33c16386c892a5512364b38b835235fe858
Don McAllister <mcallisterda@gmail.com>
2019-05-09 10:42:02 -0400

    Convert to MVC design pattern.

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,15 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Donald McAllister on 5/9/19.
+//  Copyright © 2019 Donald McAllister. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title: String = ""
+    var done: Bool = false
+    
+}
